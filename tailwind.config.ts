bc3a0cc84f1db801ffb268256f8276d07ccef2a9
Gs1lver <alebasi2005@gmail.com>
2023-04-28 00:38:19 -0300

    responsividade nav corrigida e outras coisas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,45 @@
-import type { Config  } from 'tailwindcss';
+import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+
+		theme: {
+			extend: {},
+			//tentando criar uma paleta de cores
+			textColor: {
+				'roxo': {
+					claro: '',
+					default: '#4c1b49',
+					escuro: ''
+				},
+				'lilas':{
+					claro: '',
+					default: '#9c80c2',
+					escuro: ''
+				},
+				'verde': {
+					claro: '',
+					default: '#568d33',
+					escuro: ''
+				},
+				'laranja':  {
+					claro: '',
+					default: '#ff5e1e',
+					escuro: ''
+				},
+				'amarelo': {
+					claro: '',
+					default: '#e9bf6a',
+					escuro: ''
+				},
+				'branco': {
+					claro: '',
+					default: '#ffffff',
+					escuro: ''
+				}
+
+			}
 	},
+
 	plugins: [],
 } satisfies Config;
