5e497e795bd6b6fdea2bd4dedf982786333ed386
AmeMeida <ame.pistache@gmail.com>
2023-04-29 20:49:04 -0300

    breaking: Migrando todo o controller para Prisma

--- src/pages/api/pedidos/novo.ts
+++ src/pages/api/pedidos/novo.ts
@@ -1,31 +1,44 @@
 import type { APIRoute } from "astro";
-import { pedidos } from "../../../types";
+import { Priority, PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+const toConnect = z
+  .number().int().positive()
+  .transform((value) => ({ connect: { id: value } }));
+
+const orderSchema = z.object({
+  quantity: z.number().int().positive(),
+  priority: z.nativeEnum(Priority),
+  annotations: z
+    .string()
+    .transform((value) => value || null)
+    .nullable(),
+  item: toConnect,
+  command: toConnect,
+});
 
 export const post: APIRoute = async ({ request }) => {
-    try {
-        const body = await request.json();
-        
-        if (!body.prato || (!body.horario) || !body.urgencia) {
-            throw new Error("Missing required fields");
-        }
+  const order = orderSchema.safeParse(await request.json());
+  const prisma = new PrismaClient();
 
+  if (!order.success) {
+    return new Response("Pedido inválido", {
+      status: 400,
+      statusText: "Bad Request",
+    });
+  }
 
+  try {
+    await prisma.order.create({ data: order.data });
 
-        pedidos.push({
-            prato: body.prato,
-            horario: new Date(body.horario),
-            anotacoes: body.anotacoes,
-            urgencia: body.urgencia,
-        });
-    
-        return new Response("Pedido adicionado", {
-            status: 201, statusText: "Created"
-        });
-    } catch (error) {
-        console.error(error);
-
-        return new Response("Bad request", {
-            status: 400, statusText: "Bad request"
-        });
-    }
-}
+    return new Response("Pedido adicionado", {
+      status: 201,
+      statusText: "Created",
+    });
+  } catch (error) {
+    return new Response("Erro ao adicionar pedido", {
+      status: 500,
+      statusText: "Internal Server Error",
+    });
+  }
+};
