0c87d3823cc8ae0add7035bb6aa496bf86e4929a
AmeMeida <ame.pistache@gmail.com>
2023-04-29 22:48:21 -0300

    mt formatacao

--- src/pages/api/pedidos/urgencia/[urgencia].ts
+++ src/pages/api/pedidos/urgencia/[urgencia].ts
@@ -2,34 +2,28 @@ import type { APIRoute } from "astro";
 import { Priority, PrismaClient } from "@prisma/client";
 import { z } from "zod";
 
-export const get: APIRoute = async ({ params }) => {
-    const prisma = new PrismaClient;
-    const urgencia = z.nativeEnum(Priority).safeParse(params.urgencia);
+export const get: APIRoute = async({ params }) => {
+  const prisma = new PrismaClient();
+  const urgencia = z.nativeEnum(Priority).safeParse(params.urgencia);
 
-    if (urgencia.success) {
-        const pedidos = await prisma.order.findMany({
-            where: {
-                priority: urgencia.data
-            },
-            include: {
-                item: true
-            }
-        })
+  if (!urgencia.success) {
+    return new Response(JSON.stringify(urgencia.error), {
+      status: 400,
+      statusText: "Bad Request",
+      headers: { "content-type": "application/json; charset=UTF-8" }
+    });
+  }
 
-        return new Response(JSON.stringify(pedidos), {
-            status: 200,
-            statusText: "OK",
-            headers: {
-                "content-type": "application/json; charset=UTF-8",
-            },
-        });
-    } else {
-        return new Response(JSON.stringify(urgencia.error), {
-            status: 400,
-            statusText: "Bad Request",
-            headers: {
-                "content-type": "application/json; charset=UTF-8",
-            },
-        });
+  const pedidos = await prisma.order.findMany({
+    where: { priority: urgencia.data },
+    include: {
+      item: true
     }
-}
+  });
+
+  return new Response(JSON.stringify(pedidos), {
+    status: 200,
+    statusText: "OK",
+    headers: { "content-type": "application/json; charset=UTF-8" }
+  });
+};
