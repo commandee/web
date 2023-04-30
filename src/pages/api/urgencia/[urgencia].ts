180d4fa7c5e5d37b4e171655a84024f571eb1c3d
AmeMeida <ame.pistache@gmail.com>
2023-04-30 02:55:57 -0300

    nada importante aqui

--- src/pages/api/urgencia/[urgencia].ts
+++ src/pages/api/urgencia/[urgencia].ts
@@ -1,22 +1,28 @@
 import type { APIRoute } from "astro";
-import { pedidos } from "../../../types";
+import { PrismaClient, Priority } from "@prisma/client";
+import { z } from "zod";
+
+export const prerender = true;
+
+const prisma = new PrismaClient();
+const schema = z.nativeEnum(Priority);
 
 export const get: APIRoute = ({ params }) => {
-  const urgencia = params.urgencia;
+  const urgencia = schema.safeParse(params.urgencia);
 
-  if (!urgencia)
+  if (!urgencia.success) {
     return new Response("Bad request", {
       status: 400,
       statusText: "Bad request"
     });
+  }
 
-  return new Response(
-    JSON.stringify(pedidos.filter((pedido) => pedido.urgencia === urgencia)),
-    {
-      status: 200,
-      headers: {
-        "content-type": "application/json; charset=UTF-8"
-      }
-    }
-  );
+  const pedidos = prisma.order.findMany({
+    where: { priority: urgencia.data }
+  });
+
+  return new Response(JSON.stringify(pedidos), {
+    status: 200,
+    headers: { "content-type": "application/json; charset=UTF-8" }
+  });
 };
