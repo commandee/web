0adbb406f241c2602e95559e1ed6dc9de12c0ebe
AmeMeida <ame.pistache@gmail.com>
2023-05-21 21:23:07 -0300

    n importa

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -1,9 +1,22 @@
 import { config } from "dotenv";
-import { z } from "zod";
+import { exit } from "process";
 
 config();
 
+function envAssert<T extends object, K extends keyof T>(obj: T, key: K): asserts obj is T & Record<K, string> {
+  if (!(key in obj)) {
+    console.error(`Required environment variable '${String(key)}' was not provided`);
+    exit(1);
+  }
+
+}
+
+envAssert(process.env, "DATABASE_URL");
+envAssert(process.env, "JWT_SECRET");
+
+process.env.JWT_EXPIRES_IN ??= "30s";
+
 export const jwtEnv = {
-  secret: z.string().parse(process.env.JWT_SECRET),
-  expiresIn: z.string().default("30s").parse(process.env.JWT_EXPIRES_IN)
+  secret: process.env.JWT_SECRET,
+  expiresIn: process.env.JWT_EXPIRES_IN
 };
